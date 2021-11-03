648c135d7b8e4875953fd01fee12af4cdad1409f
minki-afun <minki@a-fun.co.kr>
2021-11-03 13:13:25 +0900

    frontend and backend setting

--- Backend/src/graphql/graphqlUtils.js
+++ Backend/src/graphql/graphqlUtils.js
@@ -1,2 +1,2 @@
 import jwt from "jsonwebtoken"
-import client from "./client";
+import client from "./client"
--- Backend/src/graphql/users/user.TypeDefs.js
+++ Backend/src/graphql/users/user.TypeDefs.js
@@ -1,3 +1,23 @@
 import { gql } from "apollo-server"
 
-export default gql``
+export default gql`
+    type User {
+        id: Int!
+        email: String!
+        Content: [Content]
+        createdAt: String!
+        updatedAt: String!
+    }
+    type Content {
+        id: Int!
+        product: String!
+        price: Int!
+        text: String
+        sign: Boolean!
+        date: Int!
+        user: [User]
+        userId: Int!
+        createdAt: String!
+        updatedAt: String!
+    }
+`
